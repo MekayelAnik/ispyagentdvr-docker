b730140277304650d5f91d2c852a21b4a10c1221
Muhammad Mekayel Anik <mekayel.anik@gmail.com>
2023-08-08 03:03:22 +0600

    v4.7.4.0

--- stable-image-builder/ffmpeg.sh
+++ stable-image-builder/ffmpeg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt-get satisfy "libjxl0.7 (>= 0.7.0)" -y
+apt-get satisfy "libjxl0.7 (>= 0.7.0)" -y --no-install-recommends
 apt-get -t experimental install ffmpeg -y --no-install-recommends
 exit 0
