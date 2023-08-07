58082f073bad507b50c9411044ded5b8b9323df4
Muhammad Mekayel Anik <mekayel.anik@gmail.com>
2023-08-08 03:15:36 +0600

    v4.9.2.0

--- stable-image-builder/Agent.sh
+++ stable-image-builder/Agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 date +%c > /run-timestamp
-echo "This image was build on: $(cat /build-timestamp)"   
+cat /AgentDVR/build-timestamp
 /AgentDVR/banner.sh
 echo "This Container was started on: $(cat /run-timestamp)"
 /AgentDVR/Agent
