220084a523450e60e0424355c3433ea4b180d13c
Muhammad Mekayel Anik <mekayel.anik@gmail.com>
2023-08-08 02:42:08 +0600

    v4.3.9.0

--- stable-image-builder/agent-setup.sh
+++ stable-image-builder/agent-setup.sh
@@ -2,7 +2,7 @@
 
 # Install script for AgentDVR/ Linux
 # To execute: save and `chmod +x ./linux_setup2.sh` then `./linux_setup2.sh`
-version=4_3_8_0
+version=4_3_9_0
 
 . /etc/*-release
 arch=`uname -m`
