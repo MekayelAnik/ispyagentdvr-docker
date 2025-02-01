fb8457aa007d633ef27ab9f4a8a3a4cd0eb18366
Mohammad Mekayel Anik <mekayel.anik@gmail.com>
2025-02-01 07:45:05 +0600

    Deleted older unnecessary files and added .gitignore

--- stable-image-builder/dependency.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-	apt-get -o Acquire::Check-Valid-Until=false -o Acquire::Check-Date=false update
-	apt-get upgrade -y
-	apt-get install -y curl unzip wget
-	apt-get install -y tzdata alsa-utils libgdiplus --no-install-recommends
-exit 0
